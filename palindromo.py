f69d7e1a8f156214e32ca3f04382ab70a20f92be
Eduardo <eduardosumarivasalgado@gmail.com>
2025-11-11 12:53:50 +0100

    ejercicio palindromo añadido

---
+++ palindromo.py
@@ -0,0 +1,20 @@
+"""
+Dada una palabra o frase, di si es un palíndromo o no (se leen igual al derecho, que al reves)    
+"""
+
+
+def palindromo(frase):
+    frase_trimeada = frase.lower().replace(" ", "")
+    f = -1
+    es_palindromo = True
+    for palabra in list(frase_trimeada):
+        if palabra != list(frase_trimeada)[f]:
+            es_palindromo = False
+            break
+        f -= 1
+    return es_palindromo
+
+
+print(palindromo("Dabale arroz a la zorra el abad"))
+print(palindromo("hola"))
+print(palindromo("luz azul"))
